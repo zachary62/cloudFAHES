ea82e2e7ca0ccd8dc795d475348e3a2fd036f73c
Zachary Huang <zhuang333@wisc.edu>
2019-04-04 23:45:05 -0500

    First

---
+++ cgi-bin/RandDMVD.py
@@ -0,0 +1,178 @@
+#
+#  Zezhou Huang
+#  zhuang333@wisc.edu
+#
+import sys
+from main import sus_disguised
+
+def find_disguised_values(T, sus_dis_values,column):
+    # print(T)
+    hist = {col:T[col].value_counts() for col in T.columns}
+    # print(hist)
+    KK = []
+    # for each column in the csv
+    for k, v in hist.items():
+        if(len(hist[k]) >= 3):
+            KK.append(k)
+    if len(KK) == 1:
+        return sus_dis_values
+    Temp_T = T[KK].reset_index().values.tolist()
+    for i in range(len(Temp_T)):
+        Temp_T[i].pop(0)
+    # print(Temp_T)
+    Temp_hist = {col:T[col].value_counts() for col in KK}
+    RandDMVD_Index_T = Table_Index_RandDMVD(Temp_T)
+    for i in range(len(KK)):
+        if KK[i] != column:
+            continue
+        dis_value = None
+        largest_DV = 0
+        col_hist = hist[KK[i]]
+        most_com  = sorted(col_hist.items(), key=lambda kv: kv[1], reverse = True)
+        # print(most_com)
+        for k, v in most_com:
+            # print(k,v)
+            if v == 1:
+                break
+            k = k.lower()
+            if k == 'null':
+                continue
+            corr, PT_num_rows = subtable_correlation(Temp_T, k, i,RandDMVD_Index_T)
+            DV_Score = len(Temp_T) / PT_num_rows * corr
+            # print(corr)
+            if DV_Score > largest_DV:
+                dis_value = sus_disguised(KK[i], k, DV_Score, v, "Rand")
+                largest_DV = DV_Score
+        if dis_value is not None:
+            ratio1 = dis_value.frequency / len(Temp_T)
+            ratio2 = len(Temp_hist) / len(Temp_T)
+            # print(ratio1)
+            # print(ratio2)
+            # print(dis_value.value)
+            if ratio1 > 0.01 and ratio2 > 0.01 and dis_value.frequency > 5:
+                if dis_value not in sus_dis_values:
+                    sus_dis_values.append(dis_value)
+
+    return sus_dis_values
+
+def subtable_correlation(Temp_T, k, idx,RandDMVD_Index_T):
+    data = RandDMVD_Index_T[idx].get(k,0)
+    if data == 0:
+        print("error not find in subtable_correlation")
+        sys.exit(1)
+    # print(data)
+    RandDMVD_Index_subT = Table_Index_RandDMVD(data)
+    # print(RandDMVD_Index_subT)
+    corr = 0
+    for i in range(len(data)):
+        corr += record_correlation(data[i], Temp_T, data, RandDMVD_Index_subT, idx, RandDMVD_Index_T)
+    return corr, len(data)
+
+def record_correlation(Vec, Temp_T, data, RandDMVD_Index_subT, idx, RandDMVD_Index_T):
+    n = len(Temp_T[0])
+    for i in range(n):
+        s = Vec[i].lower()
+        if s == 'null' or s == '':
+            return 0
+    n_V_t = compute_num_cooccur(Vec, i, RandDMVD_Index_T)
+    n_vi_t = []
+    # print(Vec,idx)
+    for i in range(n):
+        if i != idx:
+            n_vi_t.append(compute_num_occur2(Vec[i], i, RandDMVD_Index_T))
+        else:
+            n_vi_t.append(0)
+
+    n_V_pt = compute_num_cooccur(Vec, idx, RandDMVD_Index_subT);
+    n_vi_pt = []
+    for i in range(n):
+        if i != idx:
+            n_vi_pt.append(compute_num_occur2(Vec[i], i, RandDMVD_Index_subT))
+        else:
+            n_vi_pt.append(0)
+
+    p_V_t = n_V_t/len(Temp_T)
+    p_V_pt = n_V_pt/len(data)
+    p_t = 1
+    p_pt = 1
+
+    for i in range(n):
+        if i != idx:
+            # print(n_vi_t[i])
+            p_t *= n_vi_t[i]/len(Temp_T)
+            p_pt *= n_vi_pt[i]/len(data)
+
+    corr_t = p_V_t / p_t
+    corr_pt = p_V_pt / p_pt
+    corr = p_V_t / (1 + pow(abs(corr_t - corr_pt), 1))
+    return corr
+
+def compute_num_occur2(v, A, RandDMVD_Index_T):
+    value = RandDMVD_Index_T[A].get(v)
+    # print(v,A,value)
+    return len(value)
+
+def compute_num_cooccur(Vec, idx, RandDMVD_Index_T):
+    # print(Vec)
+    index_min_subT = 0
+    min_subT = 0
+    first_time = True
+    num_coocur = 0
+    for i in range(len(Vec)):
+        if i != idx:
+            value = RandDMVD_Index_T[i].get(Vec[i])
+            if value == 0:
+                print("error not find in compute_num_cooccur")
+                sys.exit(1)
+            if first_time:
+                min_subT = len(value)
+                index_min_subT = i
+                first_time = False
+            else:
+                if min_subT > len(value):
+                    min_subT = len(value)
+                    index_min_subT = i
+    value = RandDMVD_Index_T[index_min_subT].get(Vec[index_min_subT])
+    for K in range(len(value)):
+        if equals(value[K], Vec, idx):
+            num_coocur += 1
+    return num_coocur
+
+def equals(V1, V2, idx):
+    if len(V1) != len(V2):
+         return False
+    for i in range(len(V1)):
+        if i == idx:
+            continue;
+        if V1[i] != V2[i]:
+            return False;
+    return True;
+
+def Table_Index_RandDMVD(Temp_T):
+    number_of_rows = len(Temp_T)
+    number_of_cols = len(Temp_T[0])
+    m_tablehist = []
+    for I in range(number_of_cols):
+        m_tablehist.append(dict())
+
+    for i in range(number_of_rows):
+        for j in range(number_of_cols ):
+            row = []
+            for ii in range(number_of_cols):
+                row.append(str(Temp_T[i][ii]).lower())
+            SS = str(Temp_T[i][j]).lower()
+            if SS == "" or SS == "null":
+                value = m_tablehist[j].get("NULL",0)
+                if value == 0:
+                    m_tablehist[j]["NULL"] = [row]
+                else:
+                    m_tablehist[j]["NULL"].append(row)
+            else:
+                value = m_tablehist[j].get(SS,0)
+                if value == 0:
+                    m_tablehist[j][SS] = [row]
+                else:
+                    m_tablehist[j][SS].append(row)
+
+
+    return m_tablehist
---
+++ cgi-bin/generate2.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3.6
+import cgi, os
+import cgitb; cgitb.enable()
+import traceback
+
+
+form = cgi.FieldStorage()
+# Get filename here.
+
+# Test if the file was uploaded
+
+try:
+   import main
+   fileitem1 = form['filename1'].value
+   date = form['date'].value
+   column = form['column'].value
+   message  = main.main2("data/" + date + "ddd" + fileitem1, column)
+
+       # fileitem3.file.seek(0)
+       # md.upload_table(tablen,fileitem3)
+
+       # message = "<p><font size=\"6\"><b>Your table has been successfully uploaded!</b></font></p>"
+       # message = message + "<p>To load your new table, enter your new table's name and click load.</p>"
+       # message = message + "<p><strong>Note:</strong> Save your old table before loading your new table or the data will be lost.</p>"
+       # message = message + "<p>Below is a preview of one tuple pair page:</p>"
+#        message = """
+#        <div class="panel panel-info">
+#            <div class="panel-heading"> You have successully uploaded the table!
+#
+#                <div class="clearfix"></div>
+#            </div>
+#            <div class="panel-body">
+#              <form enctype = "multipart/form-data"
+#                                action = "/cgi-bin/generate2.py" method = "post">
+#              <div>
+#
+#        """
+#
+#        message += html
+#        message += """             <p><input type = "submit" value = "Continue" /></p>
+#                     </form>
+#                   </div>
+#               </div>
+#               <style>
+# table {
+#   font-family: arial, sans-serif;
+#   border-collapse: collapse;
+#   width: 100%;
+# }
+#
+# td, th {
+#   border: 1px solid #dddddd;
+#   text-align: left;
+#   padding: 8px;
+# }
+#
+# tr:nth-child(even) {
+#   background-color: #dddddd;
+# }
+# </style>
+# </head>
+# <body>
+#
+# <h2>Table Preview</h2>
+#
+# <table>"""
+#        message +=  table
+#        message +=   """</table> """
+except Exception as e:
+
+   message = "<p><font size=\"6\"><b>An error has occured!</b></font></p>"
+   message = message + "<p>Please check your csv files according to the error message!</p>"
+   message = message + "<p>Key Word for the error is: " + str(e) + "</p>"
+   message = message + "<p>Detailed traceback: </p><p>" + traceback.format_exc() + "</p>"
+
+
+
+
+print ("""\
+Content-Type: text/html\n
+<html>
+""")
+# print(form)
+# print(fileitem1)
+# print(date)
+print(message)
+print("</html>")
